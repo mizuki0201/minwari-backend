e4bc2d9f2aa0c25c98709c66ebf0707b9cecfe32
mizuki-iio <mizuki.iio@di-v.co.jp>
2021-09-03 13:23:43 +0900

    ユーザー登録とログインは成功

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  # def create
-  #   user = User.new(sign_up_params)
-  #   if user.save
-  #     render_create_success
-  #   else
-  #     render_create_error
-  #   end
-  # end
+  def create
+    user = User.new(sign_up_params)
+    if user.save
+      render_create_success
+    else
+      render_create_error
+    end
+  end
 
-  # private
-  # def sign_up_params
-  #   params.require(:user).permit(:name, :user_id, :email, :phone, :password)
-  # end
+  private
+  def sign_up_params
+    params.require(:user).permit(:name, :user_id, :email, :phone, :password)
+  end
 end
